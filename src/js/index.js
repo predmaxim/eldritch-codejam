4d7547340fdc085db34dd29f7a61f6335d8f5a52
predmaxim <predmaxim@ya.ru>
2022-08-25 00:30:34 +0500

    feat: disign and disign functionality is done

---
+++ src/js/index.js
@@ -0,0 +1,7 @@
+import '../css/style.css';
+import '../assets/mythicCardBackground.png';
+import { Ancients } from "../assets/Ancients/index";
+import { rollUp, rollDown } from './slider'
+
+
+
---
+++ src/js/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+import Ancients from '../assets/Ancients/index';
+
+const arr = ancients
+console.log(arr)
